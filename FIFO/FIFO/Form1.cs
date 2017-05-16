726b945639e5a7468b6666ab18c6d1bac2ef0dc6
Paco <fbatista.fxbi@gmail.com>
2017-05-16 07:33:46 -0500

    bahahaha

--- FIFO/FIFO/Form1.cs
+++ FIFO/FIFO/Form1.cs
@@ -12,68 +12,20 @@ namespace FIFO
 {
     public partial class Form1 : Form
     {
-        private Proceso Primero;
-        private Proceso Ultimo;
-
-        private static Random r;
+        private Procesador p;
 
         public Form1()
         {
             InitializeComponent();
-            r = new Random();
         }
 
         private void bt_Comenzar_Click(object sender, EventArgs e)
         {
-            Primero = null;
-            Ultimo = null;
-
-            int ciclosVacios = 0;
-            int procesosPendientes = 0;
-            int ciclosPendientes = 0;
-
-            for (int i = 0; i < 200; i++)
-            {
-                if (r.Next(0, 4) == 0)
-                {
-                    if (Primero == null)
-                    {
-                        Primero = new Proceso();
-                        Ultimo = Primero;
-                    }
-                    else if (Primero.Siguiente == null)
-                    {
-                        Primero.Siguiente = new Proceso();
-                        Primero.Siguiente.Anterior = Primero;
-                        Ultimo = Primero.Siguiente;
-
-                    }
-                    else
-                    {
-                        Proceso temp = new Proceso();
-                        temp.Anterior = Ultimo.Anterior.Siguiente;
-                        temp.Anterior.Siguiente = temp;
-                        Ultimo = temp;
-                    }
-                    procesosPendientes++;
-                    ciclosPendientes += Ultimo.ciclosRestantes;
-                }
-                if (Primero != null)
-                {
-                    Primero.ciclosRestantes--;
-                    if (Primero.ciclosRestantes == 0)
-                    {
-                        Primero = Primero.Siguiente;
-                        procesosPendientes--;
-                    }
-                    ciclosPendientes--;
-                }
-                else
-                    ciclosVacios++;
-            }
-            tb_vacios.Text = ciclosVacios.ToString();
-            tb_procesos.Text = procesosPendientes.ToString();
-            tb_ciclos.Text = ciclosPendientes.ToString();
+            p = new Procesador();
+            p.simular(200);
+            tb_vacios.Text = p.ciclosVacios.ToString();
+            tb_procesos.Text = p.procesosPendientes.ToString();
+            tb_ciclos.Text = p.ciclosPendientes.ToString();
         }
     }
 }
---
+++ FIFO/FIFO/Procesador.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFO
+{
+    class Procesador
+    {
+        private static Random r;
+        private Proceso Primero;
+        private Proceso Ultimo;
+
+        public int ciclosVacios { get; private set; }
+        public int procesosPendientes { get; private set; }
+        public int ciclosPendientes { get; private set; }
+
+        public Procesador()
+        {
+            r = new Random();
+        }
+
+        public void simular(int ciclos)
+        {
+            for (int i = 0; i < ciclos; i++)
+            {
+                if (r.Next(0, 4) == 0)
+                {
+                    if (Primero == null)
+                    {
+                        Primero = new Proceso();
+                        Ultimo = Primero;
+                    }
+                    else if (Primero.Siguiente == null)
+                    {
+                        Primero.Siguiente = new Proceso();
+                        Primero.Siguiente.Anterior = Primero;
+                        Ultimo = Primero.Siguiente;
+
+                    }
+                    else
+                    {
+                        Proceso temp = new Proceso();
+                        temp.Anterior = Ultimo.Anterior.Siguiente;
+                        temp.Anterior.Siguiente = temp;
+                        Ultimo = temp;
+                    }
+                    procesosPendientes++;
+                    ciclosPendientes += Ultimo.ciclosRestantes;
+                }
+                if (Primero != null)
+                {
+                    Primero.ciclosRestantes--;
+                    if (Primero.ciclosRestantes == 0)
+                    {
+                        Primero = Primero.Siguiente;
+                        procesosPendientes--;
+                    }
+                    ciclosPendientes--;
+                }
+                else
+                    ciclosVacios++;
+            }
+        }
+    }
+}
